428e0899f889d27310af9b3c9e601015a697728a
MaolanaF <maolana.firmansyah.tif421@polban.ac.id>
2023-10-16 08:34:29 +0700

    Add FE Login

--- FrontEnd/react/src/App.jsx
+++ FrontEnd/react/src/App.jsx
@@ -14,6 +14,7 @@ import Mata_Kuliah from './pages/mata_kuliah/Matkul';
 import EditMatkul from './pages/mata_kuliah/EditMatkul';
 import AddPengajaran from './pages/riwayat_pengajaran/AddRiwayatPengajaran';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             <Route path="/mata_kuliah/edit/:id_matkul" element = {<EditMatkul/>} />
             <Route path="/riwayat_pengajaran/insert" element = {<AddPengajaran/>} />
             <Route path="/home" element = {<Home/>} />
+            <Route path="/login" element = {<Login/>} />
           </Routes>
       </Router>
   )
